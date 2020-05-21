0e351257b6037d6678b5c7a57d72c23b312cd1aa
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-21 17:44:48 +0200

    I added the route delete (/ courses / id) in routes.js

--- api/routes.js
+++ api/routes.js
@@ -10,5 +10,6 @@ router.get("/courses", controllers.getCourses);
 router.post("/courses", controllers.addCourse);
 router.get("/courses/:id", controllers.getCourse);
 router.put("/courses/:id", controllers.modifyCourse);
+router.delete("/courses/:id", controllers.deleteCourse);
 
 module.exports = router;
