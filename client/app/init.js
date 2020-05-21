dc7c79644f8473ee358766c648c8120bf3b94d81
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-21 19:41:43 +0200

    form, buttons and text box to show the description of the courses

--- client/app/init.js
+++ client/app/init.js
@@ -1,5 +1,5 @@
-export const init = async () => {
-  const res = await fetch('/api');
+const init = async () => {
+  const res = await fetch("/api");
   const data = await res.json();
   console.log(data);
 };
